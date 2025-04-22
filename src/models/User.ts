160593d89a1742c141e1a2ee0e97768dd12d359b
Sarrus95 <rzago@stevejobs.academy>
2025-04-22 16:28:12 +0200

    refactor(users): :recycle: Username Registration Removed
    Username is no loger required for registration (only email and pswd)

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,12 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 interface IUser extends Document {
-    username: string;
     email: string;
     password: string;
     emailAuthToken: string;
     emailVerified: boolean;
     loginAuthToken: string;
+    username: string;
     phoneNo: string;
     booksOfInterest: Schema.Types.ObjectId[];
     myBooks: Schema.Types.ObjectId[];
@@ -15,12 +15,12 @@ interface IUser extends Document {
 }
 const UserSchema = new Schema<IUser>(
   {
-    username: { type: String, required: true,unique: true},
     email: { type: String, required: true,unique : true},
     password: { type: String, required: true},
     emailAuthToken: { type: String },
     emailVerified: { type: Boolean, default: false},
     loginAuthToken: { type: String },
+    username: { type: String,unique: true},
     phoneNo: { type: String },
     booksOfInterest: [{ type: Schema.Types.ObjectId, ref: "BookOfInterest" }],
     myBooks: [{ type: Schema.Types.ObjectId, ref:"Book"}],
